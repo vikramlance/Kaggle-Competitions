21d3dc13b07e53f41833263a79936f223fabe660
Andy Miller <andymiller1313@gmail.com>
2017-03-21 22:23:55 -0500

    Added AUC ROC curve calc, commented out plotting for now.

--- CreditCardFraudDetection/Evaluation Framework.py
+++ CreditCardFraudDetection/Evaluation Framework.py
@@ -68,3 +68,95 @@ precision_score(y_true, y_pred, average='weighted')
 precision_score(y_true, y_pred, average=None)  
 
 """
+### Interesting AUP ROC Curve link:
+### http://stats.stackexchange.com/questions/21592/optimising-for-precision-recall-curves-under-class-imbalance
+
+#import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from itertools import cycle
+
+
+from sklearn import svm, datasets
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+
+
+#Omit for now
+"""
+#data import ***WILL NEED UPDATED
+data = loadData
+x = xData
+y = yData
+
+# setup plot details
+#colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
+#lw = 2
+
+# binarize the output
+y = label_binarize(y, classes=[0, 1])
+n_classes = y.shape[1]
+
+
+#train test split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.666)
+
+# Run classifier
+classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
+                                 random_state=random_state))
+y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+"""
+
+
+
+#Precision recall curve 
+precision = dict()
+recall = dict()
+average_precision = dict()
+for i in range(n_classes):
+    precision[i], recall[i], _ = precision_recall_curve(y_test[:, i],
+                                                        y_score[:, i])
+    average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
+
+# Compute micro-average ROC curve and ROC area
+precision["micro"], recall["micro"], _ = precision_recall_curve(y_test.ravel(),
+    y_score.ravel())
+average_precision["micro"] = average_precision_score(y_test, y_score,
+                                                     average="micro")
+
+
+# Plot Precision-Recall curve (this is if we want to use plt)
+"""
+plt.clf()
+plt.plot(recall[0], precision[0], lw=lw, color='navy',
+         label='Precision-Recall curve')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.ylim([0.0, 1.05])
+plt.xlim([0.0, 1.0])
+plt.title('Precision-Recall example: AUC={0:0.2f}'.format(average_precision[0]))
+plt.legend(loc="lower left")
+plt.show()
+
+# Plot Precision-Recall curve for each class
+plt.clf()
+plt.plot(recall["micro"], precision["micro"], color='gold', lw=lw,
+         label='micro-average Precision-recall curve (area = {0:0.2f})'
+               ''.format(average_precision["micro"]))
+for i, color in zip(range(n_classes), colors):
+    plt.plot(recall[i], precision[i], color=color, lw=lw,
+             label='Precision-recall curve of class {0} (area = {1:0.2f})'
+                   ''.format(i, average_precision[i]))
+
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Extension of Precision-Recall curve to multi-class')
+plt.legend(loc="lower right")
+plt.show()
+"""
